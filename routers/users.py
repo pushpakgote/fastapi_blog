346c97395d30b7511db902575e00d88990513a0f
Pushpak <pushpakgote25@gmail.com>
2026-02-21 16:32:49 +0530

    Added post edit, delete and new post creation in UI

--- routers/users.py
+++ routers/users.py
@@ -59,7 +59,7 @@ async def get_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
 
 @router.get("/{user_id}/posts", response_model=list[PostResponse])
 async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    result = await db.execute(select(models.User).where(models.User.id == user_id).order_by(models.Post.date_posted.desc()))
     user = result.scalars().first()
     if not user:
         raise HTTPException(
